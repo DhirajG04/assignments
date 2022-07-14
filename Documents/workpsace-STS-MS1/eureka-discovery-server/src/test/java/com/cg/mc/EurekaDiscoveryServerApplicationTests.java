de9d82d2924068f2483b65fe68855e9d4d992491
dhirgaik <dhirgaik@LIN24010519.corp.capgemini.com>
2022-07-14 10:53:37 +0530

    its eureka server

---
+++ Documents/workpsace-STS-MS1/eureka-discovery-server/src/test/java/com/cg/mc/EurekaDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
